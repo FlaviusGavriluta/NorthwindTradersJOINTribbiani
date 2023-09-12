0dfba3fd8736b4a68d73078287b04da437ee2903
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-09-13 02:53:52 +0300

    task3

--- task-3.sql
+++ task-3.sql
@@ -1,1 +1,7 @@
--- YOUR SOLUTION HERE
+SELECT categories.category_name AS category,
+       COUNT(product_id)        AS number_of_products
+FROM categories
+         INNER JOIN products ON categories.category_id = products.category_id
+GROUP BY category
+ORDER BY number_of_products DESC,
+         category ASC;
