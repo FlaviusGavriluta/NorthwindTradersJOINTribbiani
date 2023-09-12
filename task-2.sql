e197779c95c984c30fcd3df7d3b64586ea68f4c1
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-09-13 02:42:45 +0300

    task2

--- task-2.sql
+++ task-2.sql
@@ -1,1 +1,7 @@
--- YOUR SOLUTION HERE
+SELECT DISTINCT orders.order_id,
+       orders.freight,
+       order_details.discount
+FROM orders
+         INNER JOIN order_details ON orders.order_id = order_details.order_id
+WHERE order_details.discount > 0
+ORDER BY orders.freight DESC;
