521da790822f9a8f159afbea9e0312fddd8fb3d5
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-09-13 00:23:48 +0300

    task1

--- task-1.sql
+++ task-1.sql
@@ -1,1 +1,10 @@
--- YOUR SOLUTION HERE
+SELECT
+    products.product_name AS product,
+    suppliers.company_name AS company
+FROM
+    products
+        JOIN
+    suppliers ON products.supplier_id = suppliers.supplier_id
+ORDER BY
+    products.product_name ASC,
+    suppliers.company_name ASC;
