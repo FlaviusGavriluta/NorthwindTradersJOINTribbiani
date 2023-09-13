cbb2e6b5f1e4f35b69ea819e0f44473e593f3833
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-09-13 03:28:08 +0300

    task5

--- task-5.sql
+++ task-5.sql
@@ -1,1 +1,7 @@
--- YOUR SOLUTION HERE
+SELECT COUNT(*),
+       region.region_description
+FROM employees
+INNER JOIN employee_territories ON employees.employee_id = employee_territories.employee_id
+INNER JOIN territories ON employee_territories.territory_id = territories.territory_id
+INNER JOIN region ON territories.region_id = region.region_id
+GROUP BY region.region_description;
