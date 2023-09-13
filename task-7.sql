b0b7b7cfb857dd5b0193a88b50e807fcdbc883af
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-09-13 04:09:43 +0300

    task7

--- task-7.sql
+++ task-7.sql
@@ -1,1 +1,6 @@
--- YOUR SOLUTION HERE
+SELECT products.product_name,
+       suppliers.company_name,
+       suppliers.contact_name
+FROM products
+INNER JOIN suppliers ON products.supplier_id = suppliers.supplier_id
+ORDER BY products.product_name ASC;
