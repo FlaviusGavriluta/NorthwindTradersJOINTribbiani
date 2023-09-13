d4872e80799619740609f2edd8a80dce67c0b22c
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-09-13 04:14:42 +0300

    task8

--- task-8.sql
+++ task-8.sql
@@ -1,1 +1,7 @@
--- YOUR SOLUTION HERE
+SELECT order_details.order_id,
+       products.product_name,
+       order_details.quantity
+FROM products
+INNER JOIN order_details ON products.product_id = order_details.product_id
+ORDER BY order_details.order_id ASC,
+         order_details.quantity DESC;
