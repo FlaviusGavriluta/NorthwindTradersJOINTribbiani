00ca479b7ca692504db45ddb52a7a65b6ac4a0ab
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-09-13 03:39:55 +0300

    task6

--- task-6.sql
+++ task-6.sql
@@ -1,1 +1,6 @@
--- YOUR SOLUTION HERE
+SELECT CONCAT(employees.title_of_courtesy, ' ', employees.first_name, ' ', employees.last_name) AS employee,
+       COUNT(orders.order_id) AS orders
+FROM employees
+INNER JOIN orders ON employees.employee_id = orders.employee_id
+GROUP BY employee
+ORDER BY orders DESC;
