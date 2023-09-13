35baf544ab3afd034c5a30c02dbac668553ace1f
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-09-13 03:18:06 +0300

    task4

--- task-4.sql
+++ task-4.sql
@@ -1,1 +1,6 @@
--- YOUR SOLUTION HERE
+SELECT DISTINCT customers.company_name,
+       us_states.state_name
+FROM customers
+INNER JOIN orders ON customers.customer_id = orders.customer_id
+INNER JOIN us_states ON orders.ship_region = us_states.state_abbr
+ORDER BY customers.company_name ASC;
