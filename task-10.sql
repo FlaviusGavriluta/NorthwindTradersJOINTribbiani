98ec1f700fe6bbbecf37a5f35c189590b89b9c83
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-09-13 05:46:31 +0300

    refactor

--- task-10.sql
+++ task-10.sql
@@ -1,8 +1,8 @@
-SELECT EXTRACT(YEAR FROM order_date)                                                                                AS year,
-       EXTRACT(MONTH FROM order_date)                                                                               AS month,
-       COUNT(order_details.order_id)                                                                                AS order_count,
-       ROUND(SUM(order_details.quantity * (order_details.unit_price - order_details.unit_price *
-                                                                order_details.discount))  )                          AS revenue
+SELECT EXTRACT(YEAR FROM order_date)                                                            AS year,
+       EXTRACT(MONTH FROM order_date)                                                           AS month,
+       COUNT(order_details.order_id)                                                            AS order_count,
+       ROUND(SUM(order_details.quantity * order_details.unit_price * (1 -
+                                                                      order_details.discount))) AS revenue
 FROM orders
          INNER JOIN order_details ON orders.order_id = order_details.order_id
 WHERE EXTRACT(YEAR FROM order_date) = 1997
