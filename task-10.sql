2edb375ebc7c26b09f7c260d2bc01d76266182e1
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-09-13 05:28:03 +0300

    task10

--- task-10.sql
+++ task-10.sql
@@ -1,1 +1,9 @@
--- YOUR SOLUTION HERE
+SELECT EXTRACT(YEAR FROM order_date)                                                                                AS year,
+       EXTRACT(MONTH FROM order_date)                                                                               AS month,
+       COUNT(order_details.order_id)                                                                                AS order_count,
+       ROUND(SUM(order_details.quantity * (order_details.unit_price - order_details.unit_price *
+                                                                order_details.discount))  )                          AS revenue
+FROM orders
+         INNER JOIN order_details ON orders.order_id = order_details.order_id
+WHERE EXTRACT(YEAR FROM order_date) = 1997
+GROUP BY year, month;
--- task-9.sql
+++ task-9.sql
@@ -1,5 +1,5 @@
 SELECT products.product_name,
-       SUM(order_details.quantity * order_details.unit_price) AS sum_price
+       ROUND(SUM(order_details.quantity * order_details.unit_price)) AS sum_price
 FROM products
 INNER JOIN order_details ON products.product_id = order_details.product_id
 INNER JOIN orders ON order_details.order_id = orders.order_id
